a6643b4a84e046c00647515655a64f9a271e11d9
“midory-rojas” <midory.rojasv@gmail.com>
2025-12-16 14:43:05 +0100

    feat: Ho creato la prima rotta / che ritorna come rispostta "Server del mio blog"

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ import express from "express"; //Importo express
 const app = express(); //Invoco express
 const port = 30002; //Creo una porta 3002
 
+app.get("/", (req, res) => {
+    console.log("Server del mio blog"); 
+    res.send("Server del mio blog"); //Verifica la risposta in postman
+})
+
 app.listen(port, function () {
     console.log("Il server è in ascolto sulla porta " + port);
 })
