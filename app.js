821ce0f88f5392d1a0c71adc9a59ded8871a7aeb
“midory-rojas” <midory.rojasv@gmail.com>
2025-12-16 13:16:50 +0100

    feat: creo la porta 3002

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
-import express from "express";
+import express from "express"; //Importo express 
 
-const app = express();
+const app = express(); //Invoco express
+const port = 30002; //Creo una porta 3002
+
+app.listen(port, function () {
+    console.log("Il server è in ascolto sulla porta " + port);
+})
